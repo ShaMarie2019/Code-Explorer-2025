3932a5c373690d3995d84a2c7b40d17827437e90
ShaMarie2019 <shaniquemrock@gmail.com>
2025-06-04 16:29:44 -0400

    adding all starting code as of right now

---
+++ Module_1/Mod1_Proj.py
@@ -0,0 +1,53 @@
+# Lets create a calculator and make sure it works
+
+
+def calculator(x, y, operation):
+    #define sum, product, difference, and quotient 
+    sum = x + y
+
+    if operation == "add":
+        return sum
+    elif operation == "multiply":
+        return product
+    elif operation == "subtraction":
+        return difference
+    else:
+        return quotient
+
+ 
+
+#Call the function
+
+
+
+
+#Call the function and print 
+
+
+print(calculator())
+
+#Assign the called function to a variable 
+
+var = calculator()
+
+
+
+'''
+1. Call the calculator function with two numbers and an operation (e.g., "add", "subtract", "multiply", or "divide"), 
+and store the result in a variable (e.g., var1).
+
+
+2. Repeat Step 1 with a different set of inputs and store the result in another variable (e.g., var2).
+
+3. Call the calculator function again using the two previously stored variables as inputs, along with a new operation, 
+and store the result in a new variable (e.g., var3).
+
+'''
+
+
+
+
+
+
+
+
---
+++ Module_1/Terminal.py
@@ -0,0 +1,32 @@
+'''
+Using terminal commands try and run this code.
+
+Directories are folders 
+
+cd - Change directory 
+ls - list what is in your directory 
+mkdir - make directory
+clear - clears previous commands from terminal 
+
+
+ex: cd Desktop/Code_Explorer/Module_1              end up in Module_1
+
+ex: cd ../                                         end up in Code_Explorer
+
+Run Code:
+
+python3 filename.py
+
+'''
+
+
+
+#first run this code and see if you can get the output in your terminal
+starter = "You are "
+complements = ["amazing", "fabulous", "smart", "talented"]
+
+
+for complement in complements:
+    print(starter + complement)
+
+
